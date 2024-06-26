6b6bf64ccf7ff9036dfd28121031211261ef044e
frarosset <37275581+frarosset@users.noreply.github.com>
2024-06-27 00:51:55 +0200

    Add spin animation while the gif is being refreshed
    Since a FontAwesome icon is used for the refresh button symbol,
    the spin animation can be simply applied by toggling the '.fa-spin'
    class.
    
    Note that we want the refresh button icon to spin until the new gif
    has been loaded (and not just untild the gif data have been
    retrieved).
    
    Then, instead of changing the 'src' field of the <img> element
    immediately, a new function setImgSrc(src) has been created which
    returns a Promise, which in turn sets the 'src' attribute and
    resolves once the image has been loaded.
    
    Then, the '.fa-spin' class is removed from the refresh button icon.
    This is done in the '.finally()' method, instead of '.then()', to
    avoid the symbol to spin indefinitely in case of errors.

--- index.js
+++ index.js
@@ -3,17 +3,33 @@
 
   const img = document.querySelector("img");
   const refreshButton = document.querySelector(".refresh-button");
+  const refreshButtonIcon = document.querySelector(".refresh-button i");
 
   function getImg() {
+    refreshButtonIcon.classList.add("fa-spin");
+
     fetch(`https://api.giphy.com/v1/gifs/translate?api_key=${API_KEY}&s=cats`, {
       mode: "cors",
     })
       .then(function (response) {
-        return response.json();
+        return response.json(); // returns a Promise
       })
       .then(function (response) {
-        img.src = response.data.images.original.url;
+        return setImgSrc(response.data.images.original.url); // returns a Promise
+      })
+      .finally(function () {
+        refreshButtonIcon.classList.remove("fa-spin");
       });
+  }
+
+  function setImgSrc(src) {
+    return new Promise((resolve) => {
+      img.onload = () => {
+        resolve();
+      };
+
+      img.src = src;
+    });
   }
 
   getImg();
