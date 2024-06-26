eb6f1cbcb6cc9c2b7074dfdbeea0f23c3cb07a8d
frarosset <37275581+frarosset@users.noreply.github.com>
2024-06-27 00:21:52 +0200

    Retrieve the gif through the Giphy API
    The gif is retrieved through the Giphy API, using a public API key.
    see https://developers.giphy.com/docs/api#quick-start-guide
    
    The fetch function is used with the Giphy API URL. This returns
    a promise which resolves in an object with the representation of
    the entire HTTP response to the fetch request.
    
    The .json() method is used to extract the actual data. This returns
    a nother promise which resolves in an object ('response') with the
    representation of the entire required data.
    
    We need the response.data.images.original.url data to be used as
    the src of the img element to display the image.

---
+++ index.js
@@ -0,0 +1,19 @@
+(() => {
+  API_KEY = "KBszsvWmSCqazUTsnFOXhIwkQDQxMbK3";
+
+  const img = document.querySelector("img");
+
+  function getImg() {
+    fetch(`https://api.giphy.com/v1/gifs/translate?api_key=${API_KEY}&s=cats`, {
+      mode: "cors",
+    })
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (response) {
+        img.src = response.data.images.original.url;
+      });
+  }
+
+  getImg();
+})();
